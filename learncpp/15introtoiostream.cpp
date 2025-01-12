7dd17da386160cb1f6f283eafe20aca2de182985
vi7n <singhrob@oregonstate.edu>
2025-01-12 01:03:28 -0800

    finally pailo assignment gare

--- learncpp/15introtoiostream.cpp
+++ learncpp/15introtoiostream.cpp
@@ -1,12 +1,30 @@
+// #include <iostream> // for std::cout and std::cin
+
+// int main()
+// {
+//     std::cout << "Enter a number: "; // ask user for a number
+
+//     int x{};       // define variable x to hold user input (and value-initialize it)
+//     std::cin >> x; // get number from keyboard and store it in variable x
+
+//     std::cout << "You entered " << x << '\n';
+//     return 0;
+// }
+
 #include <iostream> // for std::cout and std::cin
 
 int main()
 {
-    std::cout << "Enter a number: "; // ask user for a number
+    std::cout << "Enter two numbers separated by a space: ";
 
-    int x{};       // define variable x to hold user input (and value-initialize it)
-    std::cin >> x; // get number from keyboard and store it in variable x
+    int x{};            // define variable x to hold user input (and value-initialize it)
+    int y{};            // define variable y to hold user input (and value-initialize it)
+    std::cin >> x >> y; // get two numbers and store in variable x and y respectively
 
-    std::cout << "You entered " << x << '\n';
+    std::cout << "You entered " << x << " and " << y << '\n';
+
     return 0;
 }
+
+// 2147483647 ,ax value jun yesle lina sakxa
+// rah yo vanda badi characterko value hale vane 0 dinxa
