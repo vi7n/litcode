7bcbf866ab2fab2a1169899e23fc5e7c80d76129
vi7n <singhrob@oregonstate.edu>
2025-01-12 16:58:51 -0800

    added function files

--- learncpp/firstcode.cpp
+++ learncpp/firstcode.cpp
@@ -12,3 +12,28 @@ int main() // forgot to add the () and also the int at the start
 }
 
 // on second trial I pressed enter after inputing each number yet it worked
+
+// function wala code:
+
+#include <iostream>
+
+// int foo()
+// {
+//     std::cout << "Enter a number";
+//     int z;
+//     std::cin >> z;
+//     return z;
+// }
+
+// int main() // forgot to add the () and also the int at the start
+// {
+//     // std::cout << "Enter 3 numbers seperated by blank space"; // forgot this semi-colon
+//     int a{foo()};
+//     int b{foo()};
+//     int c{foo()};
+//     // std::cin >> (foo()) >> (foo()) >> foo(); // wasn't sure of this line but turned out this was correct
+//     std::cout << "the number you entered are " << a << ", " << b << " and " << c;
+//     return 0;
+// }
+
+// on second trial I pressed enter after inputing each number yet it worked
