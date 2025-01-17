a1b082c41ffe573499bca047f368f19f5887a70b
vi7n <singhrob@oregonstate.edu>
2025-01-17 02:46:39 -0800

    upperlowercase added

--- learncpp/upperlowecase.cpp
+++ learncpp/upperlowecase.cpp
@@ -4,20 +4,14 @@ using namespace std;
 int main()
 {
     cout << "enter any letter \n";
-    char inpu, a_val('a'), z_val{'z'}, A_val{'A'}, Z_val{'Z'};
+    char inpu;
     cin >> inpu;
 
-    int mval, aVal, AVal, zVal, ZVal;
-    mval = inpu;
-    aVal = a_val;
-    AVal = A_val;
-    zVal = z_val;
-    ZVal = Z_val;
-    if ((mval >= AVal) && (mval <= ZVal))
+    if (inpu >= 'A' && inpu <= 'Z')
     {
         cout << "Uppercase";
     }
-    else if ((mval >= aVal) && (mval <= zVal))
+    else if (inpu >= 'a' && inpu <= 'z')
     {
         cout << "Lowercase";
     }
