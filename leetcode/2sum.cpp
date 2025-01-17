66954c236237a0aecd529691bad76fb8fd44b60b
vi7n <singhrob@oregonstate.edu>
2025-01-16 19:02:58 -0800

    more on first leetcode

--- leetcode/2sum.cpp
+++ leetcode/2sum.cpp
@@ -17,3 +17,28 @@ public:
         return {0, 0};
     }
 };
+
+// This code wasn't wotking because of the nums.size thing that I did. nums.size()-i means j will never reach the last element of the vector
+
+// class Solution {
+// public:
+//     vector<int> twoSum(vector<int>& nums, int target) {
+//         // std::cout << i;
+
+//         for (int i {0}; i < nums.size();i++){
+//             std::cout << i << std::endl;
+
+//             for (int j {i+1}; j<(nums.size()-1);j++)
+//             {
+//                 std::cout << "j:" << j << std::endl;
+
+//                 if (nums[i]+nums[j] == target)
+//                 {
+//                     return{i,j};
+//                 }
+//             }
+//         }
+//         return{0, 0};
+//     }
+
+// };
